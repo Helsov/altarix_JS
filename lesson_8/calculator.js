16acba17345ac6e1123ad149587e23bd93360340
Klepikov Vladislav <vl_klepikov@mail.ru>
2018-11-12 15:30:40 +0400

    update

--- lesson_8/calculator.js
+++ lesson_8/calculator.js
@@ -1,25 +1,5 @@
 function showResult (operand_a, operator, operand_b){
-    try {
-        switch (operator){
-            case '+':
-                var result = operand_a + operand_b;
-                break;
-            case '-':
-                var result = operand_a - operand_b;
-                break;
-            case '*':
-                var result = operand_a * operand_b;
-                break;
-            case '/':
-                var result = operand_a / operand_b;
-                break;
-            default:
-            throw console.error("Оператор должен быть: +, -, * или -");
-        };
-        return console.log(result);
-    } catch(e) {
-        console.log(e);
-    };
+    return eval(`${operand_a} ${operator} ${operand_b}`);
 };
 
 module.exports = {
--- lesson_8/script.js
+++ lesson_8/script.js
@@ -1,7 +1,7 @@
 let calculator = require('./calculator.js').showResult;
-let operand_a  = process.argv[0];
-let operator = process.argv[1];
-let operand_b = process.argv[2];
+let operand_a  = process.argv[2];
+let operator = process.argv[3];
+let operand_b = process.argv[4];
 
 
-console.log(calculator(`${operand_a}`, `${operator}`, `${operand_b}`))
+console.log(calculator(operand_a, operator, operand_b))
