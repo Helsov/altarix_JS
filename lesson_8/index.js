039a91f7c73862037960ee2d0c2b6bf9b4bb6360
Klepikov Vladislav <vl_klepikov@mail.ru>
2018-11-14 10:01:43 +0400

    Создал проект Lesson 9

--- lesson_8/index.js
+++
@@ -1,7 +0,0 @@
-let calculator = require('./calculator.js').showResult;
-let operand_a  = process.argv[2];
-let operator = process.argv[3];
-let operand_b = process.argv[4];
-
-
-console.log(calculator(operand_a, operator, operand_b))
